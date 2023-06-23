527716f910d09be8e9aac2f0fd3404cd187535d5
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-06-23 15:09:07 +0700

    cek antrain penuh lalu menambahkan if (front === -1)

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -41,7 +41,7 @@ public:
 	void remove() {
 		//cek apakah  antrian kosong
 		if (FRONT == -1) {
-			cout << "Queue underflow\n";
+			cout << "Queue underflow\n"; 
 			return;
 		}
 		cout << "\nThe element deleted from the queue is:" << queue_array[FRONT] << "\n";
