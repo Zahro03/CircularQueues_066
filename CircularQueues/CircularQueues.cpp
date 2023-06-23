c1963cfb5114acc6f3567b8d8d791d9670616c40
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-06-23 15:37:50 +0700

    menampilkan int Queues

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -107,6 +107,26 @@ int main() {
 			cout << "1. implement insert  operation" << endl;
 			cout << "2. implement delete operation" << endl;
 			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4):";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			}
 		}
 	}
 }
